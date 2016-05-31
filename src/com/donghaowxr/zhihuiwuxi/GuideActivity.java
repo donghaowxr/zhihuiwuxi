a4fd61ddbb4fea6305497294198c49c16a5644ad
董浩 <610389473@qq.com>
2016-05-31 23:52:58 +0800

    日志：完成闪屏页面(splash页面),并可以跳转到新手指导页和主页面

---
+++ src/com/donghaowxr/zhihuiwuxi/GuideActivity.java
@@ -0,0 +1,12 @@
+package com.donghaowxr.zhihuiwuxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuideActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guide);
+	}
+}
