13797771412425e8513015a9a59c70a9ca15679b
董浩 <610389473@qq.com>
2016-06-05 17:28:33 +0800

    日志：在新闻中心页面使用xUtils访问服务器，获取json数据

--- src/com/donghaowxr/zhihuiwuxi/pager/NewsPager.java
+++ src/com/donghaowxr/zhihuiwuxi/pager/NewsPager.java
@@ -1,5 +1,11 @@
 package com.donghaowxr.zhihuiwuxi.pager;
 
+import com.donghaowxr.zhihuiwuxi.global.GlobalConfig;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.Gravity;
@@ -22,5 +28,24 @@ public class NewsPager extends BasePager {
 		flContent.addView(view);
 		
 		tvTitle.setText("新闻中心");
+		
+		getDataFromServer();
+	}
+
+	private void getDataFromServer() {
+		HttpUtils utils=new HttpUtils();
+		utils.send(HttpMethod.GET, GlobalConfig.CATEGORY_URL, new RequestCallBack<String>() {
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+				String result=responseInfo.result;
+				System.out.println("服务器返回:"+result);
+			}
+
+			@Override
+			public void onFailure(HttpException error, String msg) {
+				error.printStackTrace();
+				System.out.println(msg);
+			}
+		});
 	}
 }
