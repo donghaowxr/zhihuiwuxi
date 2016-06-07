5e3c9f8c8a5a98ff411588256172fafa6dc5aaf6
董浩 <610389473@qq.com>
2016-06-07 08:46:31 +0800

    日志：添加滑动标签第三方控件

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# zhihuiwuxi
-练手用的智慧无锡app项目
-# SlidingMenu
-侧边栏开源控件
-# xUtils
-第三方开源库，用于网络请求和图片处理
+# ViewpagerIndicator
+滑动标签第三方控件
--- src/com/donghaowxr/zhihuiwuxi/global/GlobalConfig.java
+++
@@ -1,6 +0,0 @@
-package com.donghaowxr.zhihuiwuxi.global;
-
-public class GlobalConfig {
-	public static final String SERVER_URL="http://10.0.0.4:8080/zhbj";//服务器地址
-	public static final String CATEGORY_URL=SERVER_URL+"/categories.json";//分类信息地址
-}
