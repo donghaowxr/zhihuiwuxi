6acb5f432f08f8150ecf91def39fb3d7b17a4432
董浩 <610389473@qq.com>
2016-06-07 22:29:08 +0800

    日志：在指示器TabPageIndicator中重写dispatchTouchEvent()方法，请求指示器父控件不拦截子控件事件          监听新闻中心新闻页viewpager滑动事件，监听必须绑给指示器，当在第一页时允许划出侧边栏，否则不允许划出侧边栏

--- src/com/donghaowxr/zhihuiwuxi/menupager/NewsMenuDetailPager.java
+++ src/com/donghaowxr/zhihuiwuxi/menupager/NewsMenuDetailPager.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import com.donghaowxr.zhihuiwuxi.MainActivity;
 import com.donghaowxr.zhihuiwuxi.R;
 import com.donghaowxr.zhihuiwuxi.domain.NewsMenu.DataArray.ChildrenArray;
-import com.donghaowxr.zhihuiwuxi.fragment.ContentFragment;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.viewpagerindicator.TabPageIndicator;
 import android.app.Activity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NewsMenuDetailPager extends BaseMenuDetailPager {
+public class NewsMenuDetailPager extends BaseMenuDetailPager implements OnPageChangeListener {
 
 	@ViewInject(R.id.vp_news_menu_detail)
 	private ViewPager mViewPager;
@@ -51,6 +52,7 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
 		mViewPager.setAdapter(new NewsMenuDetailAdapter());
 		
 		mIndicator.setViewPager(mViewPager);
+		mIndicator.setOnPageChangeListener(this);
 	}
 	
 	private class NewsMenuDetailAdapter extends PagerAdapter{
@@ -84,6 +86,34 @@ public class NewsMenuDetailPager extends BaseMenuDetailPager {
 			container.removeView((View) object);
 		}
 		
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset,
+			int positionOffsetPixels) {
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		if (position==0) {
+			setLeftMenuState(true);
+		}else {
+			setLeftMenuState(false);
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+	}
+	
+	public void setLeftMenuState(boolean b) {
+		MainActivity activity=(MainActivity)mActivity;
+		SlidingMenu slidingMenu=activity.getSlidingMenu();
+		if (b) {
+			slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);//开启侧边栏可以滑动
+		}else {
+			slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);//关闭侧边栏不可以滑动
+		}
 	}
 
 }
