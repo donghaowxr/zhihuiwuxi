c608878ff8b128d71a32e272437db51ca0d6cd27
donghaowxr <610389473@qq.com>
2016-07-04 16:29:05 +0800

    日志：修改地图页的名称

--- src/com/donghaowxr/zhihuiwuxi/pager/GovPager.java
+++ src/com/donghaowxr/zhihuiwuxi/pager/GovPager.java
@@ -35,14 +35,6 @@ public class GovPager extends BasePager {
 
 	@Override
 	public void initData() {
-		TextView view = new TextView(mActivity);
-		view.setText("政务");
-		view.setTextColor(Color.RED);
-		view.setTextSize(22);
-		view.setGravity(Gravity.CENTER);
-
-		flContent.addView(view);
-
 		tvTitle.setText("地图");
 
 		String cache = CacheUtils.getCache(GlobalConfig.MAP_URL, mActivity, "");
