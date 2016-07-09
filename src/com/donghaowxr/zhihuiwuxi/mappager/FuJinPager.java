f6b7a36b3e7792522d15ae122111862058e53a4d
董浩 <610389473@qq.com>
2016-07-10 00:21:27 +0800

    日志：完成在城市内搜索功能

--- src/com/donghaowxr/zhihuiwuxi/mappager/FuJinPager.java
+++ src/com/donghaowxr/zhihuiwuxi/mappager/FuJinPager.java
@@ -1,19 +1,30 @@
 package com.donghaowxr.zhihuiwuxi.mappager;
 
+import java.util.ArrayList;
+
+import com.baidu.mapapi.map.MapViewLayoutParams;
+import com.baidu.mapapi.map.MapViewLayoutParams.ELayoutMode;
+import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.overlayutil.PoiOverlay;
+import com.baidu.mapapi.search.core.PoiInfo;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
 import com.baidu.mapapi.search.poi.PoiCitySearchOption;
 import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
 import com.baidu.mapapi.search.poi.PoiIndoorResult;
 import com.baidu.mapapi.search.poi.PoiResult;
 import com.baidu.mapapi.search.poi.PoiSearch;
 import com.donghaowxr.zhihuiwuxi.R;
 
+import android.R.integer;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FuJinPager extends BaseMap implements OnGetPoiSearchResultListener {
@@ -27,6 +38,38 @@ public class FuJinPager extends BaseMap implements OnGetPoiSearchResultListener 
 	private EditText etLocal;
 	private EditText etBusStart;
 	private EditText etBusStop;
+	private PoiOverlay poiOverlay;
+	private View pop;
+	private TextView tvTitle;
+	private HideThread hideThread = null;
+	private ArrayList<View>popList;
+	private class HideThread extends Thread {
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Message message = handler.obtainMessage();
+			handler.sendMessage(message);
+		}
+	}
+
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			// mMapView.removeView(pop);
+			for (int i = 0; i < popList.size(); i++) {
+				mMapView.removeView(popList.get(i));
+				popList.remove(i);
+			}
+			if (hideThread != null) {
+				hideThread.interrupt();
+				hideThread = null;
+			}
+		};
+	};
 
 	public FuJinPager(Activity activity) {
 		super(activity);
@@ -41,15 +84,48 @@ public class FuJinPager extends BaseMap implements OnGetPoiSearchResultListener 
 		etBusStop = (EditText) view.findViewById(R.id.et_bus_stop);
 		mPoiSearch = PoiSearch.newInstance();
 		mPoiSearch.setOnGetPoiSearchResultListener(this);
+		popList=new ArrayList<View>();
+		poiOverlay = new PoiOverlay(mBaiduMap) {
+			@Override
+			public boolean onPoiClick(int i) {
+				PoiInfo poiInfo = getPoiResult().getAllPoi().get(i);
+				mPoiSearch.searchPoiDetail(getSearchDetailParams(poiInfo.uid));
+				// if (pop == null) {
+				pop = View.inflate(mActivity, R.layout.item_pop, null);
+				tvTitle = (TextView) pop.findViewById(R.id.tv_pop_title);
+				MapViewLayoutParams params = createLayoutParam(poiInfo.location);
+				mMapView.addView(pop, params);
+				popList.add(pop);
+				// }else {
+				// mMapView.updateViewLayout(pop,
+				// createLayoutParam(poiInfo.location));
+				// }
+				tvTitle.setText(poiInfo.name);
+				if (hideThread == null) {
+					hideThread = new HideThread();
+					hideThread.start();
+				} else {
+					hideThread.run();
+				}
+				return true;
+			}
+		};
 		btnSearchLocal.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				mPoiSearch.searchInCity(getSearchParam(etCity.getText().toString(),etLocal.getText().toString()));
+				mPoiSearch.searchInCity(getSearchParam(etCity.getText()
+						.toString(), etLocal.getText().toString()));
 			}
-		});	
+		});
 	}
 
-	private PoiCitySearchOption getSearchParam(String mCity,String mKeyword) {
+	protected PoiDetailSearchOption getSearchDetailParams(String poiUid) {
+		PoiDetailSearchOption params = new PoiDetailSearchOption();
+		params.poiUid(poiUid);
+		return params;
+	}
+
+	private PoiCitySearchOption getSearchParam(String mCity, String mKeyword) {
 		PoiCitySearchOption mPoiCitySearchOption = new PoiCitySearchOption();
 		mPoiCitySearchOption.city(mCity);
 		mPoiCitySearchOption.keyword(mKeyword);
@@ -60,6 +136,29 @@ public class FuJinPager extends BaseMap implements OnGetPoiSearchResultListener 
 
 	@Override
 	public void onGetPoiDetailResult(PoiDetailResult result) {
+		if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+			Toast.makeText(mActivity, "没有搜索到详情", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		// if (pop == null) {
+		// pop = View.inflate(mActivity, R.layout.item_pop, null);
+		// tvTitle = (TextView) pop.findViewById(R.id.tv_pop_title);
+		// MapViewLayoutParams params = createLayoutParam(result.getLocation());
+		// mMapView.addView(pop, params);
+		// }else {
+		// mMapView.updateViewLayout(pop,
+		// createLayoutParam(result.getLocation()));
+		// }
+		// tvTitle.setText(result.getName());
+	}
+
+	private MapViewLayoutParams createLayoutParam(LatLng position) {
+		MapViewLayoutParams.Builder builder = new MapViewLayoutParams.Builder();
+		builder.layoutMode(ELayoutMode.mapMode);
+		builder.position(position);
+		builder.yOffset(-25);
+		MapViewLayoutParams params = builder.build();
+		return params;
 	}
 
 	@Override
@@ -68,11 +167,12 @@ public class FuJinPager extends BaseMap implements OnGetPoiSearchResultListener 
 
 	@Override
 	public void onGetPoiResult(PoiResult result) {
-		if (result==null||result.error!=SearchResult.ERRORNO.NO_ERROR) {
+		if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
 			Toast.makeText(mActivity, "检索失败", Toast.LENGTH_SHORT).show();
 			return;
 		}
-		PoiOverlay poiOverlay=new PoiOverlay(mBaiduMap);
+
+		mBaiduMap.setOnMarkerClickListener(poiOverlay);
 		poiOverlay.setData(result);
 		poiOverlay.addToMap();
 		poiOverlay.zoomToSpan();
